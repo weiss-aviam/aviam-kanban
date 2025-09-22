a31fd7e1511b5f02438dfe9bacf05b579ff6d4c6
Matthias WeiÃss <blanco@mail.tightpantsproductions.com>
2025-09-22 09:46:54 +0200

    fixed build and pm2 config

--- kanban-app/ecosystem.config.js
+++ kanban-app/ecosystem.config.js
@@ -2,35 +2,26 @@ module.exports = {
   apps: [
     {
       name: 'kanban-app',
-      cwd: './kanban-app',
       // Start Next.js directly so we can pass the port
-      script: '.next/standalone/server.js', // if using `output: "standalone"`
+      script: 'pnpm',
       args: 'start -p 8777',
-
       // Environment
       env: {
         NODE_ENV: 'production',
         PORT: 8777,
-        NEXT_PUBLIC_APP_URL: 'https://aviam.intranet/kanban',
-        NEXT_PUBLIC_BASE_PATH:'/kanban',
       },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 8777,
-      },
-
       // Process management
       instances: 1,          // Use 'max' to cluster across CPUs
-      exec_mode: 'fork',     // Use 'cluster' with instances>1 if desired
+      exec_mode: 'cluster',     // Use 'cluster' with instances>1 if desired
       watch: false,          // Set true only for development
       autorestart: true,
       max_restarts: 10,
       time: true,            // Timestamp logs
 
       // Optional logs (uncomment to enable)
-      // out_file: './.pm2/out.log',
-      // error_file: './.pm2/error.log',
-      // merge_logs: true,
+      out_file: './.pm2/out.log',
+      error_file: './.pm2/error.log',
+      merge_logs: true,
     },
   ],
 };
